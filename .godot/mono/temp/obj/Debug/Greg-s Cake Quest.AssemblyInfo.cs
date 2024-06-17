f1e1ccf803961088a04cffce250550697e105ed9
Naith_cz <m.raska@aragnet.cz>
2024-06-17 21:31:58 +0200

    Add PieceCake.cs

--- .godot/mono/temp/obj/Debug/Greg-s Cake Quest.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Greg-s Cake Quest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Greg-s Cake Quest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecbbaa69a90f005bdcacbea64bc195a06fa315f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c657ff9d90e929886d2cc6f2f6893be9e5894bda")]
 [assembly: System.Reflection.AssemblyProductAttribute("Greg-s Cake Quest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Greg-s Cake Quest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Scripts/mouse.cs
+++ Scripts/mouse.cs
@@ -4,38 +4,35 @@ using System.Drawing;
 
 public partial class mouse : Area2D
 {
-	private Vector2 _initialPosition;
-	private Vector2 _mouseDirection;
-	private Control _gameArea;
-	private Control _mouseControl;
+    private Vector2 _initialPosition;
+    private Control gameArea;
+    private double speed = 1.0f;
+    private RandomNumberGenerator rndGenerator = new RandomNumberGenerator();
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		_gameArea = GetNode<Control>("../GameArea");
-		_mouseControl = GetNode<Control>("./TextureRect");
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        gameArea = GetNode<Control>("../GameArea");
 
-		GD.Print($"Pozice GameArea: X = {_gameArea.Position.X}, Y = {_gameArea.Position.Y}");
-		GD.Print($"Velikost GameArea: W = {_gameArea.Size.X}, H = {_gameArea.Size.Y}");
 
-		GD.Print($"Pozice: X = {Position.X}, Y = {Position.Y}");
-	}
+        GD.Print($"Pozice GameArea: X = {gameArea.Position.X}, Y = {gameArea.Position.Y}");
+        GD.Print($"Velikost GameArea: W = {gameArea.Size.X}, H = {gameArea.Size.Y}");
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		// Animate mouse movement
-		if (Position > new Vector2(_gameArea.Size.X - _mouseControl.Size.X, _gameArea.Size.Y - _mouseControl.Size.Y))
-		{
-			Position += new Vector2(1.0f, 0.0f);
-		}
+        GD.Print($"Pozice: X = {Position.X}, Y = {Position.Y}");
+    }
 
-		if (Position < new Vector2(0.0f, 0.0f))
-		{
-			Position += new Vector2(0.0f, 1.0f);
-		}
-		else
-		{
-		}
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        if (speed < 0.0f)
+        {
+            var deltaX = rndGenerator.RandfRange(0.0f, 9.0f);
+            var deltaY = rndGenerator.RandfRange(0.0f, 9.0f);
+            GD.Print($"Pozice GameArea: X = {deltaX}, Y = {deltaY}");
+            Position = new Vector2(deltaX * 70.0f, deltaY * 70.0f);
+            speed = 1.0f;
+        }
+
+        speed -= delta;
+    }
 }
