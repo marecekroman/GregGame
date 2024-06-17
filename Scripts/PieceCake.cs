9e9941fb3e41652191dd07aa417375e096998695
Roman Mareček <marecek14@gmail.com>
2024-06-18 01:05:25 +0200

    cake position generator

--- Scripts/PieceCake.cs
+++ Scripts/PieceCake.cs
@@ -3,13 +3,33 @@ using System;
 
 public partial class PieceCake : Area2D
 {
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-    }
+	private const double spawnTimer = 2.0f;
+	private Control gameArea;
+	private double speed = spawnTimer;
+	private RandomNumberGenerator rndGenerator = new RandomNumberGenerator();
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		gameArea = GetNode<Control>("../GameArea");
+
+		/*GD.Print($"Pozice GameArea: X = {gameArea.Position.X}, Y = {gameArea.Position.Y}");
+		GD.Print($"Velikost GameArea: W = {gameArea.Size.X}, H = {gameArea.Size.Y}");
+		GD.Print($"Pozice: X = {Position.X}, Y = {Position.Y}");*/
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (speed < 0.0f)
+		{
+			var deltaX = rndGenerator.RandfRange(0.0f, 9.0f);
+			var deltaY = rndGenerator.RandfRange(0.0f, 9.0f);
+			// GD.Print($"Pozice GameArea: X = {deltaX}, Y = {deltaY}");
+			Position = new Vector2(deltaX * 70.0f, deltaY * 70.0f);
+			speed = spawnTimer;
+		}
+
+		speed -= delta;
+	}
 }
--- Scripts/mouse.cs
+++ Scripts/mouse.cs
@@ -4,35 +4,33 @@ using System.Drawing;
 
 public partial class mouse : Area2D
 {
-    private Vector2 _initialPosition;
-    private Control gameArea;
-    private double speed = 1.0f;
-    private RandomNumberGenerator rndGenerator = new RandomNumberGenerator();
+	private const double spawnTimer = 0.9f;
+	private Control gameArea;
+	private double speed = spawnTimer;
+	private RandomNumberGenerator rndGenerator = new RandomNumberGenerator();
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        gameArea = GetNode<Control>("../GameArea");
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		gameArea = GetNode<Control>("../GameArea");
 
+		/*GD.Print($"Pozice GameArea: X = {gameArea.Position.X}, Y = {gameArea.Position.Y}");
+		GD.Print($"Velikost GameArea: W = {gameArea.Size.X}, H = {gameArea.Size.Y}");
+		GD.Print($"Pozice: X = {Position.X}, Y = {Position.Y}");*/
+	}
 
-        GD.Print($"Pozice GameArea: X = {gameArea.Position.X}, Y = {gameArea.Position.Y}");
-        GD.Print($"Velikost GameArea: W = {gameArea.Size.X}, H = {gameArea.Size.Y}");
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (speed < 0.0f)
+		{
+			var deltaX = rndGenerator.RandfRange(0.0f, 9.0f);
+			var deltaY = rndGenerator.RandfRange(0.0f, 9.0f);
+			// GD.Print($"Pozice GameArea: X = {deltaX}, Y = {deltaY}");
+			Position = new Vector2(deltaX * 70.0f, deltaY * 70.0f);
+			speed = spawnTimer;
+		}
 
-        GD.Print($"Pozice: X = {Position.X}, Y = {Position.Y}");
-    }
-
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-        if (speed < 0.0f)
-        {
-            var deltaX = rndGenerator.RandfRange(0.0f, 9.0f);
-            var deltaY = rndGenerator.RandfRange(0.0f, 9.0f);
-            GD.Print($"Pozice GameArea: X = {deltaX}, Y = {deltaY}");
-            Position = new Vector2(deltaX * 70.0f, deltaY * 70.0f);
-            speed = 1.0f;
-        }
-
-        speed -= delta;
-    }
+		speed -= delta;
+	}
 }
