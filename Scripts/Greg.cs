c6feca08239f3451ad8980f4cc141f065aec9842
Naith_cz <m.raska@aragnet.cz>
2024-06-19 12:56:00 +0200

    Add Greg control over UI direction buttons

--- Scripts/Greg.cs
+++ Scripts/Greg.cs
@@ -26,12 +26,12 @@ public partial class Greg : Area2D{
             actualPosition.Y >= _gameArea.Size.Y - 70)
             return;
 
-        if (checkTileBoundary(_newPosition)) {
+        if (CheckTileBoundary(_newPosition)) {
             Position = actualPosition;
         }
     }
 
-    private bool checkTileBoundary(Vector2 target) {
+    private bool CheckTileBoundary(Vector2 target) {
         bool stop = true;
         // horizontal boundary
         if (_direction.X != 0) {
@@ -63,29 +63,35 @@ public partial class Greg : Area2D{
             return;
 
         if (@event.IsActionPressed("ui_left")) {
-            if (Position.X > 0) {
-                _newPosition = new Vector2(Position.X - 70, Position.Y);
-                _direction = new Vector2(-1, 0);
-            }
+            _leftDirection();
         }
         else if (@event.IsActionPressed("ui_right")) {
-            if (Position.X < _gameArea.Size.X - 70) {
-                _newPosition = new Vector2(Position.X + 70, Position.Y);
-                _direction = new Vector2(1, 0);
-            }
+            _rightDirection();
         }
         else if (@event.IsActionPressed("ui_up")) {
-            if (Position.Y > 0) {
-                _newPosition = new Vector2(Position.X, Position.Y - 70);
-                _direction = new Vector2(0, -1);
-            }
+            _upDirection();
         }
         else if (@event.IsActionPressed("ui_down")) {
-            if (Position.Y < _gameArea.Size.Y - 70) {
-                _newPosition = new Vector2(Position.X, Position.Y + 70);
-                _direction = new Vector2(0, 1);
-            }
+            _downDirection();
         }
+    }
+
+    private void _on_button_up_pressed() {
+        _upDirection();
+    }
+
+    private void _on_button_down_pressed() {
+        _downDirection();
+    }
+
+
+    private void _on_button_right_pressed() {
+        _rightDirection();
+    }
+
+
+    private void _on_button_left_pressed() {
+        _leftDirection();
     }
 
     public void Reset() {
@@ -96,9 +102,36 @@ public partial class Greg : Area2D{
         // Player get hit from mouse in any mode
         if (area is Mouse mouse) {
             mouse.Reset();
-            GD.Print($"Mouse catch Geg: X = {Position.X}, Y = {Position.Y}");
             Reset();
             GregCakeQuest.UpdateGregCounter(0);
+        }
+    }
+
+    private void _leftDirection() {
+        if (Position.X > 0) {
+            _newPosition = new Vector2(Position.X - 70, Position.Y);
+            _direction = new Vector2(-1, 0);
+        }
+    }
+
+    private void _downDirection() {
+        if (Position.Y < _gameArea.Size.Y - 70) {
+            _newPosition = new Vector2(Position.X, Position.Y + 70);
+            _direction = new Vector2(0, 1);
+        }
+    }
+
+    private void _upDirection() {
+        if (Position.Y > 0) {
+            _newPosition = new Vector2(Position.X, Position.Y - 70);
+            _direction = new Vector2(0, -1);
+        }
+    }
+
+    private void _rightDirection() {
+        if (Position.X < _gameArea.Size.X - 70) {
+            _newPosition = new Vector2(Position.X + 70, Position.Y);
+            _direction = new Vector2(1, 0);
         }
     }
 }
